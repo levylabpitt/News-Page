b9d0f46677d2ee48ea07a8e4fc1a615da230b30f
LevyLabJVO <jason.vo@levylab.org>
2023-12-29 03:32:30 -0500

    delete

--- src/data.js
+++
@@ -1,9 +0,0 @@
-export default function runGoogleScript(functionName){
-    return new Promise((resolve, reject) => {
-        google.script.run.withSuccessHandler( data => {
-            resolve(data)
-        }).withFailureHandler( er => {
-            reject(er)
-        })[functionName]()
-    })
-}
